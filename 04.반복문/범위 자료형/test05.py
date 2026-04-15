9ed35fc2bdea80cda5a5d73b38c39d39914c9373
seheepark2002 <hanuri507@gmail.com>
2026-04-15 23:55:06 +0900

    범위 자료형(range) 학습

---
+++ 04.반복문/범위 자료형/test05.py
@@ -0,0 +1,13 @@
+# for 반복문: 범위와 함께 사용하기
+for i in range(5):
+    print(f"{i} = 반복 변수")
+
+print()
+
+for i in range(5, 10):
+    print(f"{i} = 반복 변수")
+    
+print()
+
+for i in range(0, 10, 3):
+    print(f"{i} = 반복 변수")
---
+++ 04.반복문/범위 자료형/test08.py
@@ -0,0 +1,3 @@
+# 반대로 반복하기 2 - reversed() 함수를 사용해보자
+for i in reversed(range(5)):
+    print(f"현재 반복 변수: {i}")
